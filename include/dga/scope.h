9be0eb18a24839b749898d126e1aa49bb60a0b22
David Avedissian <git@dga.dev>
2020-01-11 22:03:42 +0000

    Finished scope guard implementation. Added tests.

--- include/dga/scope.h
+++ include/dga/scope.h
@@ -3,8 +3,26 @@
 #pragma once
 
 #include <type_traits>
+#include <exception>
 
-// Implementation of http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2019/p0052r10.pdf
+/*
+ * Implementation of http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2019/p0052r10.pdf
+ *
+ * The API here is a subset of the proposal, but types have been renamed to CamelCase to match the
+ * rest of this repo.
+ *
+ * References:
+ * - Andrei Alexandrescu "Systematic Error Handling in C++":
+ * https://www.youtube.com/watch?v=kaI4R0Ng4E8
+ * - Andrei Alexandrescu “Declarative Control Flow": https://www.youtube.com/watch?v=WjTrfoiB0MQ
+ *
+ * The difference between the 3 scope types are the following:
+ * * `ScopeExit` will always call EF at the end of the scope.
+ * * `ScopeFail` will not call EF at the end of the scope _unless_ it is constructed inside a
+ * block and an exception is thrown.
+ * * `ScopeSuccess` will call EF at the end of the scope _unless_ it is constructed inside a
+ * block and an exception is thrown.
+ */
 
 namespace dga {
 namespace detail {
@@ -14,79 +32,136 @@ template <typename T> struct remove_cvref {
 };
 template <typename T> using remove_cvref_t = typename remove_cvref<T>::type;
 
-// general scope guard.
-template <typename EF> class scope_guard {
+class OnExitPolicy {
 public:
-    template <
-        typename EFP,
-        typename std::enable_if_t<!std::is_same_v<remove_cvref_t<EFP>, scope_guard>, int> = 0,
-        typename std::enable_if_t<std::is_constructible_v<EF, EFP&>, int> = 0,
-        typename std::enable_if_t<
-            !std::is_lvalue_reference_v<EFP> && std::is_nothrow_constructible_v<EF, EFP>, int> = 0>
-    explicit scope_guard(EFP&& f) noexcept(std::is_nothrow_constructible_v<EF, EFP> ||
-                                           std::is_nothrow_constructible_v<EF, EFP&>)
-        : exit_function{std::forward<EFP>(f)} {
+    explicit OnExitPolicy(bool execute) : execute_(execute) {
     }
 
-    template <typename EFP,
-              typename std::enable_if_t<!std::is_same_v<remove_cvref_t<EFP>, scope_guard>, int> = 0,
-              typename std::enable_if_t<std::is_constructible_v<EF, EFP&>, int> = 0,
-              typename std::enable_if_t<std::is_lvalue_reference_v<EFP>, int> = 0>
-    explicit scope_guard(EFP&& f) try : exit_function(f) {
+    void release() noexcept {
+        execute_ = false;
+    }
+
+    bool shouldExecute() const noexcept {
+        return execute_;
+    }
+
+    constexpr static bool execute_on_constructor_failure = true;
+
+private:
+    bool execute_;
+};
+
+class OnFailPolicy {
+public:
+    explicit OnFailPolicy(bool execute)
+        : uncaught_on_creation_(execute ? std::uncaught_exceptions() : -1) {
+    }
+
+    void release() noexcept {
+        uncaught_on_creation_ = -1;
+    }
+
+    bool shouldExecute() const noexcept {
+        return uncaught_on_creation_ != -1 && uncaught_on_creation_ < std::uncaught_exceptions();
+    }
+
+    constexpr static bool execute_on_constructor_failure = true;
+
+private:
+    int uncaught_on_creation_;
+};
+
+class OnSuccessPolicy {
+public:
+    explicit OnSuccessPolicy(bool execute)
+        : uncaught_on_creation_(execute ? std::uncaught_exceptions() : -1) {
+    }
+
+    void release() noexcept {
+        uncaught_on_creation_ = -1;
+    }
+
+    bool shouldExecute() const noexcept {
+        return uncaught_on_creation_ != -1 && uncaught_on_creation_ >= std::uncaught_exceptions();
+    }
+
+    constexpr static bool execute_on_constructor_failure = false;
+
+private:
+    int uncaught_on_creation_;
+};
+
+template <typename EF, typename Policy> class ScopeGuardBase {
+public:
+    template <
+        typename EFP, std::enable_if_t<!std::is_same_v<remove_cvref_t<EFP>, Policy>, int> = 0,
+        std::enable_if_t<std::is_constructible_v<EF, EFP&>, int> = 0,
+        std::enable_if_t<
+            !std::is_lvalue_reference_v<EFP> && std::is_nothrow_constructible_v<EF, EFP>, int> = 0>
+    explicit ScopeGuardBase(EFP&& f) noexcept(std::is_nothrow_constructible_v<EF, EFP> ||
+                                              std::is_nothrow_constructible_v<EF, EFP&>)
+        : exit_function(std::forward<EFP>(f)), execution_policy(true) {
+    }
+
+    template <typename EFP, std::enable_if_t<!std::is_same_v<remove_cvref_t<EFP>, Policy>, int> = 0,
+              std::enable_if_t<std::is_constructible_v<EF, EFP&>, int> = 0,
+              std::enable_if_t<std::is_lvalue_reference_v<EFP>, int> = 0>
+    explicit ScopeGuardBase(EFP&& f) try : exit_function(f), execution_policy(true) {
     } catch (...) {
-        // If initialisation of exit_function throws, call f() then rethrow.
-        f();
+        // If initialisation of exit_function throws, call f() depending on the policy, then
+        // rethrow.
+        if constexpr (Policy::execute_on_constructor_failure) {
+            f();
+        }
         throw;
     }
 
-    template <typename = std::enable_if_t<std::is_nothrow_move_constructible_v<EF>>,
-              typename = std::enable_if_t<std::is_copy_constructible_v<EF>>>
-    scope_guard(scope_guard&& rhs) noexcept(std::is_nothrow_move_constructible_v<EF> ||
-                                            std::is_nothrow_copy_constructible_v<EF>) {
-        if constexpr (std::is_nothrow_move_constructible_v<EF>) {
-            exit_function = std::forward<EF>(rhs.exit_function);
-        } else {
-            exit_function = rhs.exit_function;
-        }
-        execute_on_destruction = rhs.execute_on_destruction;
+    template <
+        std::enable_if_t<
+            std::is_nothrow_move_constructible_v<EF> || std::is_copy_constructible_v<EF>, int> = 0>
+    ScopeGuardBase(ScopeGuardBase&& rhs) noexcept(std::is_nothrow_move_constructible_v<EF> ||
+                                                  std::is_nothrow_copy_constructible_v<EF>)
+        : exit_function(std::move_if_noexcept(rhs.exit_function)), execution_policy(false) {
+        execution_policy = std::move(rhs.execution_policy);
+        rhs.release();
     }
 
-    ~scope_guard() noexcept {
-        if (execute_on_destruction) {
+    ~ScopeGuardBase() noexcept(noexcept((std::declval<EF>())())) {
+        if (execution_policy.shouldExecute()) {
             exit_function();
         }
     }
 
     void release() noexcept {
-        execute_on_destruction = false;
+        execution_policy.release();
     }
 
-    scope_guard(const scope_guard&) = delete;
-    scope_guard& operator=(const scope_guard&) = delete;
-    scope_guard& operator=(scope_guard&&) = delete;
+    ScopeGuardBase(const ScopeGuardBase&) = delete;
+    ScopeGuardBase& operator=(const ScopeGuardBase&) = delete;
+    ScopeGuardBase& operator=(ScopeGuardBase&&) = delete;
 
 private:
     EF exit_function;
-    bool execute_on_destruction{true};
+    Policy execution_policy;
 };
 }  // namespace detail
 
-template <typename EF> class scope_exit : public detail::scope_guard<EF> {
+template <typename EF> class ScopeExit : public detail::ScopeGuardBase<EF, detail::OnExitPolicy> {
 public:
-    using detail::scope_guard<EF>::scope_guard;
+    using detail::ScopeGuardBase<EF, detail::OnExitPolicy>::ScopeGuardBase;
 };
+template <typename EF> ScopeExit(EF)->ScopeExit<EF>;
 
-template <typename EF> scope_exit(EF)->scope_exit<EF>;
+template <typename EF> class ScopeFail : public detail::ScopeGuardBase<EF, detail::OnFailPolicy> {
+public:
+    using detail::ScopeGuardBase<EF, detail::OnFailPolicy>::ScopeGuardBase;
+};
+template <typename EF> ScopeFail(EF)->ScopeFail<EF>;
 
-// template <typename EF> class scope_fail;
-
-// template <typename EF> class scope_success;
-
-template <typename R, typename D> class unique_resource;
-
-template <typename R, typename D, typename S = std::decay_t<R>>
-unique_resource<std::decay_t<R>, std::decay_t<D>> make_unique_resource_checked(R&& r,
-                                                                               const S& invalid,
-                                                                               D&& d) noexcept {
-}
+template <typename EF>
+class ScopeSuccess : public detail::ScopeGuardBase<EF, detail::OnSuccessPolicy> {
+public:
+    using detail::ScopeGuardBase<EF, detail::OnSuccessPolicy>::ScopeGuardBase;
+};
+template <typename EF> ScopeSuccess(EF)->ScopeSuccess<EF>;
 }  // namespace dga
--- tests/scope_test.cpp
+++ tests/scope_test.cpp
@@ -1,5 +1,158 @@
 /* Base library
  * Written by David Avedissian (c) 2018-2020 (git@dga.dev)  */
 #include <gtest/gtest.h>
+#include <dga/scope.h>
 
-TEST(SomeTest, DoesThis) {}
+namespace {
+class ThrowOnCopy {
+public:
+    ThrowOnCopy() = default;
+    ThrowOnCopy(ThrowOnCopy&&) = default;
+    ThrowOnCopy(const ThrowOnCopy&) {
+        throw 42;
+    }
+
+    void operator()() {
+        calls++;
+    }
+
+    int calls = 0;
+};
+}  // namespace
+
+TEST(ScopeExit, EndOfScope) {
+    int calls = 0;
+    {
+        auto guard = dga::ScopeExit{[&] { calls++; }};
+    }
+    EXPECT_EQ(calls, 1);
+}
+
+TEST(ScopeExit, Release) {
+    int calls = 0;
+    {
+        auto guard = dga::ScopeExit{[&] { calls++; }};
+        guard.release();
+    }
+    EXPECT_EQ(calls, 0);
+}
+
+TEST(ScopeExit, CalledOnThrow) {
+    int calls = 0;
+    try {
+        auto guard = dga::ScopeExit{[&] { calls++; }};
+        throw 42;
+    } catch (...) {
+        // empty handler.
+    }
+    EXPECT_EQ(calls, 1);
+}
+
+TEST(ScopeExit, MoveConstructor) {
+    int calls = 0;
+    {
+        auto guard1 = dga::ScopeExit{[&] { calls++; }};
+        auto guard2 = dga::ScopeExit{std::move(guard1)};
+    }
+    EXPECT_EQ(calls, 1);
+}
+
+TEST(ScopeExit, CallsFunctionWhenFunctionCopyThrows) {
+    ThrowOnCopy f;
+    bool exception_thrown = false;
+    try {
+        auto guard = dga::ScopeExit{f};
+    } catch (...) {
+        exception_thrown = true;
+    }
+    // f will throw an exception when copied into the scope object. When this happens, f should be
+    // called.
+    EXPECT_TRUE(exception_thrown);
+    EXPECT_EQ(f.calls, 1);
+}
+
+TEST(ScopeFail, EndOfScope) {
+    int calls = 0;
+    {
+        auto guard = dga::ScopeFail{[&] { calls++; }};
+    }
+    EXPECT_EQ(calls, 0);
+}
+
+TEST(ScopeFail, Release) {
+    int calls = 0;
+    try {
+        auto guard = dga::ScopeFail{[&] { calls++; }};
+        guard.release();
+    } catch (...) {
+        // empty handler.
+    }
+    EXPECT_EQ(calls, 0);
+}
+
+TEST(ScopeFail, CalledOnThrow) {
+    int calls = 0;
+    try {
+        auto guard = dga::ScopeFail{[&] { calls++; }};
+        throw 42;
+    } catch (...) {
+        // empty handler.
+    }
+    EXPECT_EQ(calls, 1);
+}
+
+TEST(ScopeFail, CallsFunctionWhenFunctionCopyThrows) {
+    ThrowOnCopy f;
+    bool exception_thrown = false;
+    try {
+        auto guard = dga::ScopeFail{f};
+    } catch (...) {
+        exception_thrown = true;
+    }
+    // f will throw an exception when copied into the scope object. When this happens, f should be
+    // called.
+    EXPECT_TRUE(exception_thrown);
+    EXPECT_EQ(f.calls, 1);
+}
+
+TEST(ScopeSuccess, EndOfScope) {
+    int calls = 0;
+    {
+        auto guard = dga::ScopeSuccess{[&] { calls++; }};
+    }
+    EXPECT_EQ(calls, 1);
+}
+
+TEST(ScopeSuccess, Release) {
+    int calls = 0;
+    {
+        auto guard = dga::ScopeSuccess{[&] { calls++; }};
+        guard.release();
+    }
+    EXPECT_EQ(calls, 0);
+}
+
+TEST(ScopeSuccess, CalledOnThrow) {
+    int calls = 0;
+    try {
+        auto guard = dga::ScopeSuccess{[&] { calls++; }};
+        throw 42;
+    } catch (...) {
+        // empty handler.
+    }
+    EXPECT_EQ(calls, 0);
+}
+
+TEST(ScopeSuccess, DoesntCallFunctionWhenFunctionCopyThrows) {
+    ThrowOnCopy f;
+    bool exception_thrown = false;
+    try {
+        auto guard = dga::ScopeSuccess{f};
+    } catch (...) {
+        exception_thrown = true;
+    }
+    // f will throw an exception when copied into the scope object. When this happens, f should not
+    // have been called.
+    EXPECT_TRUE(exception_thrown);
+    EXPECT_EQ(f.calls, 0);
+}
