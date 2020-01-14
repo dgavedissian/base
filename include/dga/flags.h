c05d549d7f646416fc6b1410d75b431d0ce5f5ca
David Avedissian <git@dga.dev>
2020-01-14 13:39:58 +0000

    Finished implementing bitflags.

--- include/dga/flags.h
+++ include/dga/flags.h
@@ -4,184 +4,177 @@
 
 #include "../dga/aliases.h"
 
+#include <bitset>
+#include <type_traits>
+
+/*
+ * A type-safe collection of flags, that avoids having to work with bitmasking and shifting to
+ * assign and retrieve flags. Additionally, enums that use this class don't require to be specified
+ * in powers of two.
+ *
+ * An unchecked precondition of this class is that all flag values in `EnumType` are numbered from 0
+ * to n, where n is the number of flags.
+ */
+
 namespace dga {
 namespace detail {
-template <typename Enum> struct EnumTraits {
-    static constexpr std::size_t size() {
-        return 1;
-    }
+// An enum type is a flag type if it has a _Count value
+template <typename EnumType, typename = void> struct is_flags_enum : std::false_type {};
+
+template <typename EnumType>
+struct is_flags_enum<EnumType, decltype(static_cast<void>(EnumType::_Count))>
+    : std::is_enum<EnumType> {
+    static constexpr auto size = static_cast<std::size_t>(EnumType::_Count);
 };
+}  // namespace detail
 
-template <int Size> struct FlagSetMaskType { using type = u32; };
-
-template <typename Enum> class FlagsImpl {
+template <typename EnumType> class Flags {
 public:
-    using enum_traits = detail::EnumTraits<Enum>;
-    using mask_type = typename detail::FlagSetMaskType<enum_traits::size()>::type;
+    using mask_type = std::make_unsigned_t<std::underlying_type_t<EnumType>>;
+    static_assert(detail::is_flags_enum<EnumType>::value,
+                  "EnumType must be an 'enum' or 'enum class' that contains a _Count member.");
 
-    static constexpr FlagsImpl all() {
-        return FlagsImpl(mask_type((mask_type(1) << enum_traits::size()) - mask_type(1)));
+    static constexpr Flags all() {
+        return Flags(
+            mask_type((mask_type(1) << detail::is_flags_enum<EnumType>::size) - mask_type(1)));
     }
 
-    static constexpr FlagsImpl none() {
-        return FlagsImpl(mask_type(0));
+    static constexpr Flags none() {
+        return Flags(mask_type(0));
     }
 
-    constexpr explicit FlagsImpl(Enum e) noexcept : value_(mask(e)) {
+    // constructors.
+    constexpr Flags() noexcept : value_(0) {
     }
 
-    constexpr FlagsImpl(const FlagsImpl& rhs) noexcept : value_(rhs.value_) {
+    constexpr explicit Flags(EnumType e) noexcept : value_(mask(e)) {
     }
 
-    constexpr explicit FlagsImpl(mask_type flags) noexcept : value_(flags) {
+    constexpr Flags(const Flags& rhs) noexcept : value_(rhs.value_) {
     }
 
-    constexpr FlagsImpl set(Enum e) const {
-        return FlagsImpl(value_ | mask(e));
+    constexpr explicit Flags(mask_type flags) noexcept : value_(flags) {
     }
 
-    constexpr FlagsImpl reset(Enum e) const {
-        return FlagsImpl(value_ & ~mask(e));
+    // operations.
+    void set(EnumType e) {
+        value_ |= mask(e);
     }
 
-    constexpr FlagsImpl toggle(Enum e) const {
-        return FlagsImpl(value_ ^ mask(e));
+    void reset(EnumType e) {
+        value_ &= ~mask(e);
     }
 
-    constexpr FlagsImpl toggle_all() const {
-        return FlagsImpl(value_ ^ all().to_int());
+    void toggle(EnumType e) {
+        value_ ^= mask(e);
     }
 
-    constexpr mask_type to_int() const {
+    void toggleAll() {
+        value_ ^= all().value_;
+    }
+
+    constexpr bool isSet(EnumType e) const {
+        return (value_ & mask(e)) != 0;
+    }
+
+    constexpr mask_type value() const noexcept {
         return value_;
     }
 
-    constexpr bool is_set(Enum e) const {
-        return (value_ & mask(e)) != mask_type(0u);
-    }
-
-    constexpr FlagsImpl bitwise_or(const FlagsImpl& other) const {
-        return FlagsImpl(value_ | other.value_);
-    }
-
-    constexpr FlagsImpl bitwise_xor(const FlagsImpl& other) const {
-        return FlagsImpl(value_ ^ other.value_);
-    }
-
-    constexpr FlagsImpl bitwise_and(const FlagsImpl& other) const {
-        return FlagsImpl(value_ & other.value_);
-    }
-
-    /*
-    FlagsImpl& operator|=(FlagsImpl const& rhs) noexcept {
+    // operators.
+    Flags& operator|=(const Flags& rhs) noexcept {
         value_ |= rhs.value_;
         return *this;
     }
 
-    FlagsImpl& operator&=(FlagsImpl const& rhs) noexcept {
+    Flags& operator&=(const Flags& rhs) noexcept {
         value_ &= rhs.value_;
         return *this;
     }
 
-    FlagsImpl& operator^=(FlagsImpl const& rhs) noexcept {
+    Flags& operator^=(const Flags& rhs) noexcept {
         value_ ^= rhs.value_;
         return *this;
     }
 
-    constexpr FlagsImpl operator|(FlagsImpl const& rhs) const noexcept {
-        return FlagsImpl(value_ | rhs.value_);
+    constexpr Flags operator|(const Flags& rhs) const noexcept {
+        return Flags(value_ | rhs.value_);
     }
 
-    constexpr FlagsImpl operator&(FlagsImpl const& rhs) const noexcept {
-        return FlagsImpl(value_ & rhs.value_);
+    constexpr Flags operator|(EnumType rhs) const noexcept {
+        return Flags(value_ | mask(rhs));
     }
 
-    constexpr FlagsImpl operator^(FlagsImpl const& rhs) const noexcept {
-        return FlagsImpl(value_ ^ rhs.value_);
+    constexpr Flags operator&(const Flags& rhs) const noexcept {
+        return Flags(value_ & rhs.value_);
+    }
+
+    constexpr Flags operator&(EnumType rhs) const noexcept {
+        return Flags(value_ & mask(rhs));
+    }
+
+    constexpr Flags operator^(const Flags& rhs) const noexcept {
+        return Flags(value_ ^ rhs.value_);
+    }
+
+    constexpr Flags operator^(EnumType rhs) const noexcept {
+        return Flags(value_ ^ mask(rhs));
     }
 
     constexpr bool operator!() const noexcept {
         return !value_;
     }
 
-    constexpr FlagsImpl operator~() const noexcept {
-        return FlagsImpl(value_ ^ mask_type(all()));
+    constexpr Flags operator~() const noexcept {
+        return Flags(value_ ^ all().value_);
     }
 
-    constexpr bool operator==(FlagsImpl const& rhs) const noexcept {
+    constexpr bool operator==(const Flags& rhs) const noexcept {
         return value_ == rhs.value_;
     }
 
-    constexpr bool operator!=(FlagsImpl const& rhs) const noexcept {
+    constexpr bool operator==(EnumType rhs) const noexcept {
+        return value_ == mask(rhs);
+    }
+
+    constexpr bool operator!=(const Flags& rhs) const noexcept {
         return value_ != rhs.value_;
     }
-     */
+
+    constexpr bool operator!=(EnumType rhs) const noexcept {
+        return value_ != mask(rhs);
+    }
 
 private:
     mask_type value_;
 
-    static mask_type mask(Enum e) {
+    static mask_type mask(EnumType e) {
         return mask_type(mask_type(1u) << std::size_t(e));
     }
 };
-}  // namespace detail
 
-template <typename Enum> class FlagCombination : public detail::FlagsImpl<Enum> {
-    using detail::FlagsImpl<Enum>::FlagsImpl;
-};
-
-template <typename Enum> class FlagMask : public detail::FlagsImpl<Enum> {
-    using detail::FlagsImpl<Enum>::FlagsImpl;
-};
-
-template <typename Enum>
-FlagCombination<Enum> operator|(const FlagCombination<Enum>& lhs,
-                                const FlagCombination<Enum>& rhs) {
-    return lhs.bitwise_or(rhs);
+template <typename EnumType>
+constexpr Flags<EnumType> operator|(EnumType e, Flags<EnumType> const& flags) noexcept {
+    return flags | e;
 }
 
-template <typename Enum>
-FlagCombination<Enum> operator|(const FlagCombination<Enum>& lhs, const Enum& rhs) {
-    return lhs.bitwise_or(FlagCombination<Enum>(rhs));
+template <typename EnumType>
+constexpr Flags<EnumType> operator&(EnumType e, Flags<EnumType> const& flags) noexcept {
+    return flags & e;
 }
 
-template <typename Enum>
-FlagCombination<Enum> operator&(const FlagCombination<Enum>& lhs,
-                                const FlagCombination<Enum>& rhs) {
-    return lhs.bitwise_and(rhs);
+template <typename EnumType>
+constexpr Flags<EnumType> operator^(EnumType e, Flags<EnumType> const& flags) noexcept {
+    return flags ^ e;
 }
 
-template <typename Enum>
-FlagCombination<Enum> operator&(const FlagCombination<Enum>& lhs, const Enum& rhs) {
-    return lhs.bitwise_and(FlagCombination<Enum>(rhs));
+template <typename EnumType>
+constexpr bool operator==(EnumType e, Flags<EnumType> const& flags) noexcept {
+    return flags == e;
 }
 
-template <typename Enum>
-
-/*
-template <typename Enum>
-constexpr Flags<Enum> operator|(Enum bit, Flags<Enum> const& flags) noexcept {
-    return flags | bit;
+template <typename EnumType>
+constexpr bool operator!=(EnumType e, Flags<EnumType> const& flags) noexcept {
+    return flags != e;
 }
-
-template <typename Enum>
-constexpr Flags<Enum> operator&(Enum bit, Flags<Enum> const& flags) noexcept {
-    return flags & bit;
-}
-
-template <typename Enum>
-constexpr Flags<Enum> operator^(Enum bit, Flags<Enum> const& flags) noexcept {
-    return flags ^ bit;
-}
-
-template <typename Enum>
-constexpr bool operator==(Enum bit, Flags<Enum> const& flags) noexcept {
-    return flags == bit;
-}
-
-template <typename Enum>
-constexpr bool operator!=(Enum bit, Flags<Enum> const& flags) noexcept {
-    return flags != bit;
-}
- */
 }  // namespace dga
--- tests/flags_test.cpp
+++ tests/flags_test.cpp
@@ -4,13 +4,151 @@
 #include <dga/flags.h>
 
 namespace {
-enum class TestEnum {
-    Value0, Value1, Value2, _Count
-};
+enum class TestEnum { A, B, C, _Count };
+
+enum TestUnscopedEnum { A, B, C, _Count };
+
+enum class IntEnum : short { A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, _Count };
 }  // namespace
 
-using dga::FlagsImpl;
+using dga::Flags;
+
+TEST(Flags, DefaultConstruct) {
+    Flags<TestEnum> flags;
+    EXPECT_FALSE(flags.isSet(TestEnum::A));
+    EXPECT_FALSE(flags.isSet(TestEnum::B));
+    EXPECT_FALSE(flags.isSet(TestEnum::C));
+}
+
+TEST(Flags, ConstructWithEnum) {
+    Flags<TestEnum> flags{TestEnum::A};
+    EXPECT_TRUE(flags.isSet(TestEnum::A));
+    EXPECT_FALSE(flags.isSet(TestEnum::B));
+    EXPECT_FALSE(flags.isSet(TestEnum::C));
+}
+
+TEST(Flags, CopyConstructWithEnum) {
+    // enum value constructor is explicit, so we cannot use copy-initialization directly.
+    Flags<TestEnum> flags = Flags{TestEnum::A};
+    EXPECT_TRUE(flags.isSet(TestEnum::A));
+    EXPECT_FALSE(flags.isSet(TestEnum::B));
+    EXPECT_FALSE(flags.isSet(TestEnum::C));
+}
+
+TEST(Flags, All) {
+    auto all_flags = Flags<TestEnum>::all();
+    EXPECT_TRUE(all_flags.isSet(TestEnum::A));
+    EXPECT_TRUE(all_flags.isSet(TestEnum::B));
+    EXPECT_TRUE(all_flags.isSet(TestEnum::C));
+    EXPECT_EQ(all_flags, Flags<TestEnum>{TestEnum::A} | TestEnum::B | TestEnum::C);
+}
+
+TEST(Flags, None) {
+    auto none_flags = Flags<TestEnum>::none();
+    EXPECT_FALSE(none_flags.isSet(TestEnum::A));
+    EXPECT_FALSE(none_flags.isSet(TestEnum::B));
+    EXPECT_FALSE(none_flags.isSet(TestEnum::C));
+    EXPECT_EQ(none_flags, Flags<TestEnum>());
+}
 
 TEST(Flags, Set) {
+    Flags<TestEnum> flags;
+    EXPECT_FALSE(flags.isSet(TestEnum::A));
+    EXPECT_FALSE(flags.isSet(TestEnum::B));
+    EXPECT_FALSE(flags.isSet(TestEnum::C));
 
+    flags.set(TestEnum::B);
+    EXPECT_FALSE(flags.isSet(TestEnum::A));
+    EXPECT_TRUE(flags.isSet(TestEnum::B));
+    EXPECT_FALSE(flags.isSet(TestEnum::C));
+
+    flags.set(TestEnum::A);
+    EXPECT_TRUE(flags.isSet(TestEnum::A));
+    EXPECT_TRUE(flags.isSet(TestEnum::B));
+    EXPECT_FALSE(flags.isSet(TestEnum::C));
+
+    flags.set(TestEnum::A);
+    EXPECT_TRUE(flags.isSet(TestEnum::A));
+    EXPECT_TRUE(flags.isSet(TestEnum::B));
+    EXPECT_FALSE(flags.isSet(TestEnum::C));
+}
+
+TEST(Flags, Reset) {
+    Flags<TestEnum> flags{TestEnum::B};
+    EXPECT_FALSE(flags.isSet(TestEnum::A));
+    EXPECT_TRUE(flags.isSet(TestEnum::B));
+    EXPECT_FALSE(flags.isSet(TestEnum::C));
+
+    flags.reset(TestEnum::A);
+    EXPECT_FALSE(flags.isSet(TestEnum::A));
+    EXPECT_TRUE(flags.isSet(TestEnum::B));
+    EXPECT_FALSE(flags.isSet(TestEnum::C));
+
+    flags.reset(TestEnum::B);
+    EXPECT_FALSE(flags.isSet(TestEnum::A));
+    EXPECT_FALSE(flags.isSet(TestEnum::B));
+    EXPECT_FALSE(flags.isSet(TestEnum::C));
+}
+
+TEST(Flags, Toggle) {
+    Flags<TestEnum> flags{TestEnum::C};
+    EXPECT_FALSE(flags.isSet(TestEnum::A));
+    EXPECT_FALSE(flags.isSet(TestEnum::B));
+    EXPECT_TRUE(flags.isSet(TestEnum::C));
+
+    flags.toggle(TestEnum::B);
+    EXPECT_FALSE(flags.isSet(TestEnum::A));
+    EXPECT_TRUE(flags.isSet(TestEnum::B));
+    EXPECT_TRUE(flags.isSet(TestEnum::C));
+
+    flags.toggleAll();
+    EXPECT_TRUE(flags.isSet(TestEnum::A));
+    EXPECT_FALSE(flags.isSet(TestEnum::B));
+    EXPECT_FALSE(flags.isSet(TestEnum::C));
+}
+
+TEST(Flags, Operators) {
+    {
+        auto flags1 = Flags{TestEnum::A} | TestEnum::B;
+        auto flags2 = TestEnum::A | Flags{TestEnum::B};
+        EXPECT_TRUE(flags1.isSet(TestEnum::A));
+        EXPECT_TRUE(flags1.isSet(TestEnum::B));
+        EXPECT_FALSE(flags1.isSet(TestEnum::C));
+        EXPECT_TRUE(flags2.isSet(TestEnum::A));
+        EXPECT_TRUE(flags2.isSet(TestEnum::B));
+        EXPECT_FALSE(flags2.isSet(TestEnum::C));
+        EXPECT_EQ(flags1, flags2);
+    }
+    {
+        auto conjunction = Flags<TestEnum>::all() & TestEnum::B;
+        EXPECT_EQ(conjunction, TestEnum::B);
+        EXPECT_EQ(TestEnum::B, conjunction);
+    }
+    {
+        auto toggle = Flags<TestEnum>::all() ^ TestEnum::B;
+        EXPECT_EQ(toggle, Flags{TestEnum::A} | TestEnum::C);
+    }
+    {
+        auto flags = Flags{TestEnum::A} | TestEnum::C;
+        EXPECT_EQ(~flags, TestEnum::B);
+        EXPECT_EQ(~Flags<TestEnum>::all(), Flags<TestEnum>::none());
+    }
+}
+
+TEST(Flags, UnscopedEnum) {
+    Flags<TestUnscopedEnum> flags;
+    flags.set(A);
+    flags.set(B);
+    EXPECT_TRUE(flags.isSet(TestUnscopedEnum::A));
+    EXPECT_TRUE(flags.isSet(TestUnscopedEnum::B));
+    EXPECT_FALSE(flags.isSet(TestUnscopedEnum::C));
+}
+
+TEST(Flags, IntOverflow) {
+    static_assert(sizeof(Flags<IntEnum>) == 2);
+    static_assert(std::is_same_v<typename Flags<IntEnum>::mask_type, unsigned short>);
+
+    Flags<IntEnum> flags;
+    flags.set(IntEnum::A15);
+    EXPECT_TRUE(flags.isSet(IntEnum::A15));
 }
